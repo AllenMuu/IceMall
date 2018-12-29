e2b2ec90f96420fca479547c437c571a86e2f066
qiaomu <qiaomu@vichain.com>
2018-12-29 16:48:38 +0800

    项目搭建

---
+++ ice-auth/src/main/java/com/ice/auth/AuthApplication.java
@@ -0,0 +1,20 @@
+package com.ice.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author qiaomu666@163.com
+ * @since 2018-12-29
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
+
